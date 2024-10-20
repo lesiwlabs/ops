ec5fbe9f93939057460553d9ab4ab827b11706f2
Chris Lesiw <chris@chrislesiw.com>
2024-10-19 20:32:35 -0400

    golang: Add missing path in find command

--- golang/ops.go
+++ golang/ops.go
@@ -25,7 +25,8 @@ func (Ops) Test() {
 func (Ops) Lint() {
 	Runner().MustRun(GolangCi(), "run")
 	if !GoModReplaceAllowed {
-		r := Runner().MustGet("find", "-type", "f", "-name", "go.mod", "-exec",
+		r := Runner().MustGet("find", ".", "-type", "f", "-name", "go.mod",
+			"-exec",
 			"grep", "-n", "^replace", "go.mod", "/dev/null", ";")
 		if r.Out != "" {
 			panic("replace directive found in go.mod\n" + r.Out)
